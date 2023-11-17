33d61fecea5df4747e6729f5f4eff63140516f52
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-17 18:50:00 -0300

    add: export of the BASE_URL

--- src/utils/getDataFromDB.js
+++ src/utils/getDataFromDB.js
@@ -1,9 +1,8 @@
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL
 /**
  * @param {string} url
  */
 export async function getDataFromDB(url) {
-  const BASE_URL = import.meta.env.VITE_BACKEND_URL
-
   try {
     const response = await fetch(`${BASE_URL}${url}`)
     if (!response.ok) throw new Error(`HTTP Error! Status: ${response.status}`)
