29f5dbe036ee29b93471aa969e84ae8085afbacd
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-21 18:16:56 -0300

    update: delete and get teams

--- src/components/TeamRow.jsx
+++ src/components/TeamRow.jsx
@@ -2,7 +2,7 @@ import { PropTypes } from 'prop-types'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
-import useDeleteTeam from '../hooks/useDeleteTeam'
+import { deleteTeam } from '../services/api'
 
 TeamRow.propTypes = {
   team: PropTypes.shape({
@@ -16,31 +16,33 @@ TeamRow.propTypes = {
 
 function TeamRow({ team, setUpdateTeams }) {
   const { id, name, tla, country } = team
-  const { onDeleteTeam } = useDeleteTeam()
 
-  function handleDelete() {
-    onDeleteTeam(id)
+  async function handleDelete() {
+    await deleteTeam(id)
+
     setUpdateTeams(prev => !prev)
   }
 
   return (
-    <tr>
-      <td className='p-4 py-2'>{id}</td>
-      <td className='p-4 py-2 text-start'>{name}</td>
-      <td className='p-4 py-2 hidden md:table-cell'>{tla}</td>
-      <td className='p-4 py-2 hidden md:table-cell'>{country}</td>
-      <td className='p-4 py-2 flex justify-center gap-4'>
-        <Link to={`/s/${id}`}>
-          <FontAwesomeIcon icon={faEye} size='lg' />
-        </Link>
-        <Link to={`/s/${id}/edit`}>
-          <FontAwesomeIcon icon={faEdit} size='lg' />
-        </Link>
-        <button onClick={handleDelete}>
-          <FontAwesomeIcon icon={faTrash} size='lg' />
-        </button>
-      </td>
-    </tr>
+    team && (
+      <tr>
+        <td className='p-4 py-2'>{id}</td>
+        <td className='p-4 py-2 text-start'>{name}</td>
+        <td className='p-4 py-2 hidden md:table-cell'>{tla}</td>
+        <td className='p-4 py-2 hidden md:table-cell'>{country}</td>
+        <td className='p-4 py-2 flex justify-center gap-4'>
+          <Link to={`/s/${id}`}>
+            <FontAwesomeIcon icon={faEye} size='lg' />
+          </Link>
+          <Link to={`/s/${id}/edit`}>
+            <FontAwesomeIcon icon={faEdit} size='lg' />
+          </Link>
+          <button onClick={handleDelete}>
+            <FontAwesomeIcon icon={faTrash} size='lg' />
+          </button>
+        </td>
+      </tr>
+    )
   )
 }
 
