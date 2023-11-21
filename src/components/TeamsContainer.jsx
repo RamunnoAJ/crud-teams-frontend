10c32d45d49d76f88e3782291b60f5ad8b26be92
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-21 18:17:28 -0300

    update: TeamsContainer to render only if there are teams

--- src/components/TeamsContainer.jsx
+++ src/components/TeamsContainer.jsx
@@ -8,22 +8,29 @@ TeamsContainer.propTypes = {
 
 function TeamsContainer({ teams, setUpdateTeams }) {
   return (
-    <table className='w-full text-center table-auto border-separate border-spacing-x-1 bg-slate-800 text-slate-300 shadow-sm border-2 border-y-8 md:border-8 border-slate-800 mb-8'>
-      <thead className='font-semibold bg-slate-700'>
-        <tr>
-          <th className='p-4 py-2'>ID</th>
-          <th className='p-4 py-2'>Name</th>
-          <th className='p-4 py-2 hidden md:table-cell'>TLA</th>
-          <th className='p-4 py-2 hidden md:table-cell'>Country</th>
-          <th className='p-4 py-2'>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {teams.map(team => (
-          <TeamRow key={team.id} team={team} setUpdateTeams={setUpdateTeams} />
-        ))}
-      </tbody>
-    </table>
+    teams && (
+      <table className='w-full text-center table-auto border-separate border-spacing-x-1 bg-slate-800 text-slate-300 shadow-sm border-2 border-y-8 md:border-8 border-slate-800 mb-8'>
+        <thead className='font-semibold bg-slate-700'>
+          <tr>
+            <th className='p-4 py-2'>ID</th>
+            <th className='p-4 py-2'>Name</th>
+            <th className='p-4 py-2 hidden md:table-cell'>TLA</th>
+            <th className='p-4 py-2 hidden md:table-cell'>Country</th>
+            <th className='p-4 py-2'>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams &&
+            teams.map(team => (
+              <TeamRow
+                key={team.id}
+                team={team}
+                setUpdateTeams={setUpdateTeams}
+              />
+            ))}
+        </tbody>
+      </table>
+    )
   )
 }
 
