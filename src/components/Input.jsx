42c3fb2f1d914abf9eacce0a3da76fe4ffd45272
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-28 14:22:59 -0300

    add: edit team functionallity

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,13 +4,20 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   labelText: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 }
 
-function Input({ name, type, labelText }) {
+function Input({ name, type, labelText, onChange }) {
   return (
     <div className='flex flex-col w-full gap-2 max-w-xs'>
       <label htmlFor={name}>{labelText}:</label>
-      <input type={type} name={name} id={name} />
+      <input
+        className='px-4 py-2 rounded bg-slate-100 text-black'
+        type={type}
+        name={name}
+        id={name}
+        onChange={onChange}
+      />
     </div>
   )
 }
--- src/pages/FormEdit.jsx
+++ src/pages/FormEdit.jsx
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types'
 import Input from '../components/Input'
 import useGetTeamByID from '../hooks/useGetTeamByID'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { send } from '../services/api'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
 
 FormEdit.propTypes = {
   team: PropTypes.shape({
@@ -25,35 +28,116 @@ FormEdit.propTypes = {
 function FormEdit() {
   const { id } = useParams()
   const { team } = useGetTeamByID(id)
+  const [teamData, setTeamData] = useState({})
+
+  function handleChange(e) {
+    setTeamData(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const teamUpdated = {
+      ...team,
+      ...teamData,
+      lastUpdated: new Date().toISOString().split('T')[0],
+    }
+
+    try {
+      await send(id, teamUpdated, 'PATCH')
+      toast.success('Team updated successfully')
+    } catch (error) {
+      toast.error('Error updating team')
+    }
+  }
 
   return (
-    <form className='flex flex-col justify-center items-center gap-2'>
-      <h1>
-        Edit team: #{id} - {team.name}
-      </h1>
+    <>
+      <Link to='/'>Back</Link>
+      <form
+        className='flex flex-col justify-center items-center gap-2 py-8'
+        onSubmit={handleSubmit}>
+        <h1>
+          Edit team: #{id} - {team.name}
+        </h1>
 
-      <Input name='name' type='text' placeholder={team.name} labelText='Name' />
-      <Input
-        name='country'
-        type='text'
-        placeholder={team.country}
-        labelText='Country'
-      />
-      <Input
-        name='shortName'
-        type='text'
-        placeholder={team.shortName}
-        labelText='Short Name'
-      />
-      <Input name='tla' type='text' labelText='Tla' />
-      <Input name='image' type='file' labelText='Image' />
-      <Input
-        name='address'
-        type='text'
-        placeholder={team.address}
-        labelText='Address'
-      />
-    </form>
+        <Input
+          name='name'
+          type='text'
+          placeholder={team.name}
+          labelText='Name'
+          onChange={handleChange}
+        />
+        <Input
+          name='country'
+          type='text'
+          placeholder={team.country}
+          labelText='Country'
+          onChange={handleChange}
+        />
+        <Input
+          name='shortName'
+          type='text'
+          placeholder={team.shortName}
+          labelText='Short Name'
+          onChange={handleChange}
+        />
+        <Input name='tla' type='text' labelText='Tla' onChange={handleChange} />
+        <Input
+          name='image'
+          type='file'
+          labelText='Image'
+          onChange={handleChange}
+        />
+        <Input
+          name='address'
+          type='text'
+          placeholder={team.address}
+          labelText='Address'
+          onChange={handleChange}
+        />
+        <Input
+          name='phone'
+          type='text'
+          labelText='Phone'
+          onChange={handleChange}
+        />
+        <Input
+          name='email'
+          type='email'
+          labelText='Email'
+          onChange={handleChange}
+        />
+        <Input
+          name='website'
+          type='text'
+          labelText='Website'
+          onChange={handleChange}
+        />
+        <Input
+          name='founded'
+          type='number'
+          labelText='Founded'
+          onChange={handleChange}
+        />
+        <Input
+          name='Stadium'
+          type='text'
+          labelText='Stadium'
+          onChange={handleChange}
+        />
+        <Input
+          name='colors'
+          type='text'
+          labelText='Colors'
+          onChange={handleChange}
+        />
+
+        <button type='submit'>Submit</button>
+      </form>
+    </>
   )
 }
 
