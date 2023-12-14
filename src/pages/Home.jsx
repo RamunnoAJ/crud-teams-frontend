a7ce2413d7d13793e07f05dfe0c0af9bc482c373
RamunnoAJ <ramunnoagustin@gmail.com>
2023-12-14 17:21:13 -0300

    add: button text and display number of teams

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import ResetButton from '../components/ResetButton'
 import TeamsContainer from '../components/TeamsContainer'
 import useGetTeams from '../hooks/useGetTeams'
+import AddButton from '../components/AddButton'
 
 function Home() {
   const { teams, setUpdateTeams } = useGetTeams()
@@ -13,7 +14,11 @@ function Home() {
   return (
     <main className='flex flex-col justify-center items-center gap-4 p-6 mx-auto max-w-7xl bg-[#262626] w-full text-neutral-200'>
       <h1 className='text-3xl md:text-5xl font-bold uppercase'>Teams</h1>
-      <ResetButton setUpdateTeams={setUpdateTeams} />
+      <div className='flex flex-row gap-4 items-center justify-between '>
+        <p>There are {teams.length} teams</p>
+        <ResetButton setUpdateTeams={setUpdateTeams} />
+        <AddButton />
+      </div>
       <TeamsContainer teams={teams} setUpdateTeams={setUpdateTeams} />
     </main>
   )
